073da5cceb3fb4b4bb4efc35a3dbe31420c5c88c
ralphy andrade <moiselk211@gmail.com>
2022-06-21 17:47:53 +0200

    subo nuevo ejercicio

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import ListaDetareas from './componentes/lista-de-tareas';
+
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+     <div className='tarea-lista-principal'>
+        <h1> Mis Tareas</h1>
+    <ListaDetareas/>
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
